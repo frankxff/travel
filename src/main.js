fcede73652159bea1d3152baefb731252fd6f92d
A6F68F3CBF5C83E1707AA42D08AF5002 <A6F68F3CBF5C83E1707AA42D08AF5002@i-search.com.cn>
2019-05-27 11:30:03 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
 import './registerServiceWorker'
+import '../public/styles/reset.css'
+import '../public/styles/border.css'
+import '../src/assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import HomeHeader from '../components/Header/Header'
+
+export default {
+    name: 'Main',
+    components: {
+        HomeHeader
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    
+</style>
